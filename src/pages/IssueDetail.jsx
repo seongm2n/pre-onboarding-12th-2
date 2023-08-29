5f72ed58a54e441d70c452a571d802553bf22fa1
seongmin <seongm22n@gmail.com>
2023-08-30 03:11:14 +0900

    feat : 본문, 아바타까지 보이는 상세 페이지 생성

--- src/pages/IssueDetail.jsx
+++ src/pages/IssueDetail.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getIssueDetail } from '../utils/apis/issues';
+import IssueItem from '../components/IssueItem';
 
 function IssueDetail() {
-	return <div>issue detail</div>;
+	const { id } = useParams();
+	const [issue, setIssue] = useState(null);
+
+	useEffect(() => {
+		const fetchIssueDetail = async () => {
+			const detail = await getIssueDetail(id);
+			setIssue(detail);
+		};
+		fetchIssueDetail();
+	}, [id]);
+
+	if (!issue) {
+		return <p>Loading...</p>;
+	}
+
+	return (
+		<div>
+			{issue ? (
+				<div>
+					<div>
+						<img
+							src={issue.user?.avatar_url}
+							alt='깃헙 프로필 이미지'
+						/>
+					</div>
+					<IssueItem issue={issue} />
+					<span>{issue.body}</span>
+				</div>
+			) : (
+				<p>Loading...</p>
+			)}
+		</div>
+	);
 }
 
 export default IssueDetail;
