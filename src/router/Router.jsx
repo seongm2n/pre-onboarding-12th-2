153a8e4fa8ee42f0f755e3969acf191b56604808
seongmin <seongm22n@gmail.com>
2023-08-30 03:06:07 +0900

    fix : router경로 수정

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,7 +3,7 @@ import NotFound from '../pages/NotFound';
 import IssueList from '../pages/IssueList';
 import IssueDetail from '../pages/IssueDetail';
 import { Root } from '../router/Root';
-import { OWNER, REPO } from '../utils/apis/issues';
+import { API_URL } from '../utils/urls/url';
 
 export const router = createBrowserRouter([
 	{
@@ -13,7 +13,11 @@ export const router = createBrowserRouter([
 		children: [
 			{
 				index: true,
-				element: <Navigate to={`/repos/${OWNER}/${REPO}/issues`} />,
+				element: (
+					<Navigate
+						to={`/repos/${API_URL.organization}/${API_URL.repository}/issues`}
+					/>
+				),
 			},
 			{ path: '/repos/:owner/:repo/issues', element: <IssueList /> },
 			{ path: '/repos/:owner/:repo/issues/:id', element: <IssueDetail /> },
