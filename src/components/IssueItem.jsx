c61ec030d072f7042f53af0821b8aa68294c74cc
seongmin <seongm22n@gmail.com>
2023-08-30 19:33:48 +0900

    style : styled-components 추가

--- src/components/IssueItem.jsx
+++ src/components/IssueItem.jsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import React from 'react';
 import dateToKorea from '../utils/dateToKorea';
 
@@ -5,16 +6,44 @@ function IssueItem({ issue }) {
 	const { number, title, comments } = issue;
 
 	return (
-		<li>
-			<div>
-				<h2>{`#${number} ${title}`}</h2>
+		<Layout>
+			<Info>
+				<h1>{`#${number} ${title}`}</h1>
 				<p>
-					작성자: {issue.user?.login}, 작성일: {dateToKorea(issue.created_at)}
+					작성자: {issue.user.login}, 작성일: {dateToKorea(issue.created_at)}
 				</p>
-				<span>{`코멘트: ${comments}`}</span>
-			</div>
-		</li>
+			</Info>
+			<Comment>{`코멘트: ${comments}`}</Comment>
+		</Layout>
 	);
 }
 
 export default IssueItem;
+
+const Layout = styled.li`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 15px 0 15px 0;
+	border-bottom: 1px solid black;
+`;
+
+const Info = styled.div`
+	display: flex;
+	flex-direction: column;
+
+	h1 {
+		font-size: 1rem;
+		margin-bottom: 10px;
+		width: 480px;
+	}
+
+	p {
+		font-size: 0.8rem;
+	}
+`;
+
+const Comment = styled.span`
+	font-size: 0.8rem;
+	display: block;
+`;
--- src/pages/IssueDetail.jsx
+++ src/pages/IssueDetail.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
 import { getIssueDetail } from '../utils/apis/issues';
 import IssueItem from '../components/IssueItem';
+import MarkdownPreview from '@uiw/react-markdown-preview';
+import Loading from '../components/Layout/Loading';
 
 function IssueDetail() {
 	const { id } = useParams();
@@ -16,27 +19,68 @@ function IssueDetail() {
 	}, [id]);
 
 	if (!issue) {
-		return <p>Loading...</p>;
+		return <Loading />;
 	}
 
 	return (
-		<div>
+		<Container>
 			{issue ? (
-				<div>
-					<div>
-						<img
+				<IssueContainer>
+					<Info>
+						<ProfileImage
 							src={issue.user?.avatar_url}
 							alt='깃헙 프로필 이미지'
 						/>
-					</div>
-					<IssueItem issue={issue} />
-					<span>{issue.body}</span>
-				</div>
+
+						<IssueItem issue={issue} />
+					</Info>
+					{issue.body && (
+						<MarkdownPreviewContainer>
+							<MarkdownPreview
+								source={issue.body}
+								wrapperElement={{ 'data-color-mode': 'light' }}
+							/>
+						</MarkdownPreviewContainer>
+					)}
+				</IssueContainer>
 			) : (
-				<p>Loading...</p>
+				<Loading />
 			)}
-		</div>
+		</Container>
 	);
 }
 
 export default IssueDetail;
+
+const Container = styled.div`
+	display: flex;
+	justify-content: center;
+`;
+
+const IssueContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin-top: 20px;
+`;
+
+const Info = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+const ProfileImage = styled.img`
+	width: 45px;
+	height: 45px;
+	margin-right: 20px;
+`;
+
+const MarkdownPreviewContainer = styled.div`
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	width: 600px;
+	margin: 10px;
+`;
