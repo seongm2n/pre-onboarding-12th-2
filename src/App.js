4cc33e451610c3bfb9a37ab3e12769e3f575642c
seongmin <seongm22n@gmail.com>
2023-08-30 19:39:28 +0900

    feat :IssueProvider 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
-import './App.css';
+import GlobalStyle from './styles/GlobalStyle';
 import { router } from './router/Router';
+import { IssueProvider } from './context/IssueContext';
 
 function App() {
 	return (
-		<>
+		<IssueProvider>
+			<GlobalStyle />
 			<RouterProvider router={router} />
-		</>
+		</IssueProvider>
 	);
 }
 export default App;
